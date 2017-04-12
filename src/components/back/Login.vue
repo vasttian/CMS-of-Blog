9cfbc82ce7852fd9637d5949148cbc343ca14f9c
tianyiming <tianyiming26@gmail.com>
2017-04-12 19:20:54 +0800

    fix Login.vue

--- src/components/back/Login.vue
+++ src/components/back/Login.vue
@@ -7,62 +7,67 @@
       <p class="input">
         <i class="fa fa-user fa-fw"></i>
         <input id="userName"
-               type="text"
-               name="userName"
-               placeholder="用户名"
-               v-model.trim="name"
+          type="text"
+          name="userName"
+          placeholder="用户名"
+          v-model.trim="name"
         >
       </p>
       <p class="input">
         <i class="fa fa-key fa-fw"></i>
         <input id="password"
-               type="password"
-               placeholder="密码"
-               v-model.trim="pwd"
+          type="password"
+          placeholder="密码"
+          v-model.trim="pwd"
         >
       </p>
       <p class="info">{{info}}</p>
       <p>
-        <button @click="doLogin()">登陆
+        <button @click="doLogin()">登录
         </button>
       </p>
     </div>
   </section>
 </template>
 <script>
-  import {mapActions} from 'vuex'
-  import {set}  from '../../assets/js/cookieUtil'
+  import { mapActions } from 'vuex';
+  import { set }  from '../../assets/js/cookieUtil';
 
   export default{
-    data(){
+    data() {
       return {
         name: '',
         pwd: '',
-        info: ''
+        info: '',
       }
     },
     methods: {
-      doLogin(){
-        if (!this.name.length) return this.info = '请输入正常的用户名'
-        if (!this.pwd.length) return this.info = '请输入正常的密码'
+      doLogin() {
+        if (!this.name.length) {
+          return this.info = '请输入正常的用户名';
+        }
+
+        if (!this.pwd.length) {
+          return this.info = '请输入正常的密码';
+        }
 
         this.login({name: this.name, pwd: this.pwd})
           .then(() => {
-            const date = new Date(Date.now() + 60000 * 30)
-            set('user', this.name, date, '/', window.location.hostname)
-            this.$router.push({path: '/console'})
+            const date = new Date(Date.now() + 60000 * 30);
+            set('user', this.name, date, '/', window.location.hostname);
+            this.$router.push({ path: '/console' });
           })
-          .catch(msg => this.info = msg)
+          .catch(msg => this.info = msg);
       },
-      clearInfo(){
-        this.info = ''
+      clearInfo() {
+        this.info = '';
       },
-      ...mapActions(['login'])
+      ...mapActions(['login']),
     },
     watch: {
       name: 'clearInfo',
-      pwd: 'clearInfo'
-    }
+      pwd: 'clearInfo',
+    },
   }
 </script>
 <style lang="sass" rel="stylesheet/scss" scoped>
