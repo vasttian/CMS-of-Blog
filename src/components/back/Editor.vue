cc430f5ef4fa2089ccb96b62a150cafb1a362ded
tianyiming <tianyiming26@gmail.com>
2017-04-13 11:11:04 +0800

    fix Editor.vue

--- src/components/back/Editor.vue
+++ src/components/back/Editor.vue
@@ -1,85 +1,93 @@
 <template>
   <section class="editor">
     <input class="title"
-           placeholder="标题"
-           v-model="title">
-    <div :class="inspected?'inspect':'edit'">
+      placeholder="标题"
+      v-model="title">
+    <div :class="inspected ? 'inspect' : 'edit'">
       <textarea v-model="content" spellcheck="false"></textarea>
       <button class="toggle"
-              @click="inspected = !inspected">
+        @click="inspected = !inspected">
         <i class="fa fa-chevron-left fa-fw"
-           v-show="!inspected"></i>
+          v-show="!inspected"></i>
         <i class="fa fa-chevron-right fa-fw"
-           v-show="inspected"></i>
+          v-show="inspected"></i>
       </button>
       <article id="a" v-html="markedContent"></article>
     </div>
     <div class="panel">
       <button class="saveArticle"
-              @click="save">保存
+        @click="save">保存
       </button>
     </div>
   </section>
 </template>
 <script>
-  import {mapActions, mapMutations} from 'vuex'
-  import marked     from '../../assets/js/marked.min'
-  import hljs       from '../../assets/js/highlight.pack'
+  import { mapActions, mapMutations } from 'vuex';
+  import marked from '../../assets/js/marked.min';
+  import hljs from '../../assets/js/highlight.pack';
 
-  export default{
-    data(){
+  export default {
+    data() {
       return {
         inspected: false,
-        markedContent: ''
+        markedContent: '',
       }
     },
-    created(){
-      const id = this.$route.query.id
-      if (id) return this.getArticle(id)
-      this.SET_ARTICLE({date: new Date()})
+    created() {
+      const id = this.$route.query.id;
+      if (id) {
+        return this.getArticle(id);
+      }
+
+      this.SET_ARTICLE({ date: new Date(), });
     },
-    updated(){
-      this.highlight()
+    updated() {
+      this.highlight();
     },
     methods: {
-      save(){
+      save() {
         this.saveArticle()
           .then(() => this.$router.push({name: 'articles'}))
-          .catch(err => console.log(err))
+          .catch(err => console.log(err));
       },
-      highlight(){
+      highlight() {
         setTimeout(() => {
-          hljs.initHighlighting.called = false
-          hljs.initHighlighting()
-        }, 0)
+          hljs.initHighlighting.called = false;
+          hljs.initHighlighting();
+        }, 0);
       },
-      ...mapActions(['getArticle', 'saveArticle']),
-      ...mapMutations(['SET_ARTICLE'])
+      ...mapActions([
+        'getArticle',
+        'saveArticle',
+      ]),
+      ...mapMutations([
+        'SET_ARTICLE',
+      ]),
     },
     computed: {
       content: {
-        get(){
+        get() {
           this.markedContent = marked(
             this.$store.state.article.content || '',
-            {sanitize: true}
+            { sanitize: true  }
           )
-          this.highlight()
-          return this.$store.state.article.content
+          this.highlight();
+          return this.$store.state.article.content;
         },
-        set(value){
-          this.$store.commit('UPDATE_CONTENT', value)
+        set(value) {
+          this.$store.commit('UPDATE_CONTENT', value);
         }
       },
       title: {
-        get(){
-          return this.$store.state.article.title
+        get() {
+          return this.$store.state.article.title;
         },
-        set(value){
-          this.$store.commit('UPDATE_TITLE', value)
-        }
-      }
-    }
-  }
+        set(value) {
+          this.$store.commit('UPDATE_TITLE', value);
+        },
+      },
+    },
+  };
 </script>
 <style lang="sass" rel="stylesheet/scss">
   @import "../../style/mixins.scss";
